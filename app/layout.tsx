65b42819d25e640a22ded1442f20d35cd8686d5f
Rahul Khandait <90828823+rahulkhandait-sde@users.noreply.github.com>
2024-12-01 00:07:56 +0530

    file updated

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rahul's Portfolio",
+  description: "Modern & Minimal Rahul Khandait's Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+		<html lang='en' suppressHydrationWarning>
+			<head>
+				<link
+					rel='icon'
+					href='\public\RahulKhndait-Img.png'
+					sizes='any'
+				/>
+			</head>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='dark'
+					enableSystem
+					disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
